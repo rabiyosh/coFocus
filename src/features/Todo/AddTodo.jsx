4c9b671c309193da6c2072a2e21e09eb96ac4319
rabiyosh <iamrabbiya@gmail.com>
2026-05-07 09:44:47 +0500

    Final maybe

--- src/features/Todo/AddTodo.jsx
+++ src/features/Todo/AddTodo.jsx
@@ -2,44 +2,51 @@ import React, { useEffect, useState } from "react";
 import Inputs from "@/shared/ui/Input/Inputs";
 import Button from "@/shared/ui/Button/Button";
 
-function AddTodo() {
-    const [title, setTitle] = useState("");
-    const [text, setText] = useState("");
-    const [todos, setTodos] = useState(() => {
-        const savedTodos = JSON.parse(localStorage.getItem("todos"));
-        return savedTodos || [];
-    });
-    (useEffect(() => {
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }),
-        [todos]);
+function AddTodo({ todos, setTodos, onSubmit }) {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    return savedTodos || [];
+  });
+  (useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }),
+    [todos]);
 
-    function addTodos() {
-        if (title.trim() === "" && text.trim() === "") return;
-    }
+  function addTodos() {
+    if (title.trim() === "" && text.trim() === "") return;
 
-    const time = new Date().toLocaleDateString("ru-RU");
+    
+    const newTodo = { id: Date.now(), title: title, text: text, done: false };
 
-    return (
-        <div>
-            <h1>
-                TODAY <span>{time}</span>{" "}
-            </h1>
-            <Inputs
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Enter task's title"
-                className=" title_input"
-            />
-            <textarea
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Enter task "
-                className=" task_textarea"
-            ></textarea>
-            <Button className="submit_button">Submit</Button>
-        </div>
-    );
+    setTodos([...todos, newTodo]);
+    setTitle("");
+    setText("");
+  }
+
+  const time = new Date().toLocaleDateString("ru-RU");
+
+  return (
+    <div>
+      <h1>
+        TODAY <span>{time}</span>{" "}
+      </h1>
+      <Inputs
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter task's title"
+        className=" title_input"
+      />
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter task "
+        className=" task_textarea"
+      ></textarea>
+      <Button className="submit_button">Submit</Button>
+    </div>
+  );
 }
 
 export default AddTodo;
--- src/widgets/MiniTodoWidget/MiniTodoWidget.jsx
+++ src/widgets/MiniTodoWidget/MiniTodoWidget.jsx
@@ -1,9 +1,35 @@
-import "./MiniTodoWidget.scss"
+import { useState } from "react";
+import "./MiniTodoWidget.scss";
+import Modal from "@/shared/ui/popup/Modal";
 
 function MiniTodoWidget() {
+  const [isListOpen, setIsListOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   return (
-    <div  className="glassWidgets todo_widget">ggg</div>
-  )
+    <div
+      className="glassWidgets todo_widget"
+      onClick={() => setIsListOpen(true)}
+    >
+      <h1>ToDo List</h1>
+
+      <Modal
+        isOpen={isListOpen}
+        onClose={() => setIsListOpen(false)}
+        title="ToDo List"
+      >
+        <p>Здесь будут задачи</p>
+        <button
+          onClick={() => {
+            setIsListOpen(false);
+            setIsFormOpen(true);
+          }}
+        >
+          +
+        </button>
+      </Modal>
+    </div>
+  );
 }
 
-export default MiniTodoWidget
+export default MiniTodoWidget;
